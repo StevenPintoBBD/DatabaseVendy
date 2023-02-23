83852f2d4b54ca6575746c21454c046483a52b2f
Steven Pinto <steven.pinto@vcontractor.co.za>
2023-02-23 10:43:27 +0200

    Added query to see all tables.

---
+++ AllTables.sql
@@ -0,0 +1,28 @@
+USE VendyDB
+GO
+
+-- This will test the table creation
+
+SELECT * FROM VendingMachine;
+GO
+
+SELECT * FROM Item;
+GO
+
+SELECT * FROM ItemCategory;
+GO
+
+SELECT * FROM Transactions;
+GO
+
+SELECT * FROM Employee;
+GO
+
+SELECT * FROM Branch;
+GO
+
+SELECT * FROM BranchLocation;
+GO
+
+SELECT * FROM Room;
+GO
