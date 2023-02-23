b28a4f36b012c11072b9741b57d6763ff5d480bd
Stephen Potgieter <122513930+StephenPotgieter@users.noreply.github.com>
2023-02-23 09:48:26 +0000

    Update table creation queries.sql

--- table creation queries.sql
+++ table creation queries.sql
@@ -5,66 +5,66 @@ USE VendyDB
 GO
 
 CREATE TABLE VendingMachine(
-	Vmachine_ID int identity (1,1) not null,
-	Room_ID int null,
+	VmachineId int identity (1,1) not null,
+	RoomId int null,
 	Capacity int null,
-	Total_revenue float null,
-	constraint pk_VendingMachine primary key clustered (Vmachine_ID asc)
+	TotalRevenue float null,
+	constraint pk_VendingMachine primary key clustered (VmachineId asc)
 );
 
 
 CREATE TABLE Room(
-	Room_ID int identity (1,1) not null,
-	Branch_ID int null,
-	Room_name varchar(100) null,
-	Room_floor int null,
-	constraint pk_Room primary key clustered (Room_ID asc)
+	RoomId int identity (1,1) not null,
+	BranchId int null,
+	RoomName varchar(100) null,
+	RoomFloor int null,
+	constraint pk_Room primary key clustered (RoomId asc)
 );
 
 CREATE TABLE Branch (
-	Branch_ID int identity (1,1) not null,
-	Branch_name varchar(100) null,
+	BranchId int identity (1,1) not null,
+	BranchName varchar(100) null,
 	Postcode varchar(10) null,
-	constraint pk_Branch primary key clustered (Branch_ID asc)
+	constraint pk_Branch primary key clustered (BranchId asc)
 );
 
 CREATE TABLE BranchLocation (
 	Postcode varchar(10) not null,
 	City varchar(100) null,
-	constraint pk_BranchLocation primary key clustered (PostCode)
+	constraint pk_BranchLocation primary key clustered (Postcode)
 );
 
 
 CREATE TABLE Item (
-	Item_ID int identity (1,1) not null,
-	Vmachine_ID int null,
-	Item_name varchar(100),
+	ItemId int identity (1,1) not null,
+	VmachineId int null,
+	ItemName varchar(100),
 	Price float null,
 	Category_ID int null,
 	Quantity int null,
-	Item_position varchar(2),
-	constraint pk_Item primary key clustered (Item_ID asc)
+	ItemPosition varchar(2),
+	constraint pk_Item primary key clustered (ItemId asc)
 );
 
 CREATE TABLE ItemCategory (
-	Category_ID int identity (1,1) not null,
-	Category_name varchar(50) null,
-	constraint pk_ItemCategory primary key clustered (Category_ID asc)
+	CategoryId int identity (1,1) not null,
+	CategoryName varchar(50) null,
+	constraint pk_ItemCategory primary key clustered (CategoryId asc)
 );
 
 CREATE TABLE Transactions (
-	Transaction_ID int identity (1,1) not null,
-	Transaction_date date null, 
-	Item_ID int null,
-	Employee_ID int null,
-	constraint pk_Transactions primary key clustered (Transaction_ID asc)
+	TransactionId int identity (1,1) not null,
+	TransactionDate date null, 
+	ItemId int null,
+	EmployeeId int null,
+	constraint pk_Transactions primary key clustered (TransactionId asc)
 );
 
 CREATE TABLE Employee (
-	Employee_ID int identity (1,1) not null,
-	First_name varchar(50) null,
-	Last_name varchar(50) null, 
+	EmployeeId int identity (1,1) not null,
+	FirstName varchar(50) null,
+	LastName varchar(50) null, 
 	Allocated_credits int null,
-	constraint pk_Employee primary key clustered (Employee_ID)
+	constraint pk_Employee primary key clustered (EmployeeId)
 );
 
