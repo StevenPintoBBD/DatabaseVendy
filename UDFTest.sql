51d03c6b292170df2d87b3e63eb00e1826196917
Steven Pinto <steven.pinto@vcontractor.co.za>
2023-02-23 19:56:41 +0200

    Added UDF test

--- UDFTest.sql
+++ UDFTest.sql
@@ -4,3 +4,7 @@ GO
 SELECT * FROM dbo.udfMostTransactionOnDatePerVmachine();
 Go
 
+SELECT FirstName, LastName, dbo.udfGetMostBoughtItemPerEmployee(2) AS MostBoughtItem
+FROM Employee
+WHERE EmployeeId=2;
+GO
